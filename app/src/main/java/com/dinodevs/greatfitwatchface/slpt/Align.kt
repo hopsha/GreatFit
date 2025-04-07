5bbc68dd1a9aea7fbdf4dc3b0794f036abdf5650
siarh <siarhei.hopsha@gmail.com>
2025-04-07 22:34:24 +0200

    Create Slpt Align helpers

---
+++ app/src/main/java/com/dinodevs/greatfitwatchface/slpt/Align.kt
@@ -0,0 +1,13 @@
+package com.dinodevs.greatfitwatchface.slpt
+
+enum class AlignX(val slptValue: Byte) {
+    LEFT(0),
+    RIGHT(1),
+    CENTER(2),
+}
+
+enum class AlignY(val slptValue: Byte) {
+    TOP(0),
+    BOTTOM(1),
+    CENTER(2),
+}
